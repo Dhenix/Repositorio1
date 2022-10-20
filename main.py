5d473c993c29a82bfecf5649acc88547c48b2ed6
Dhenix <116231111+Dhenix@users.noreply.github.com>
2022-10-20 00:52:59 +0000

    aprendi a criar variaveis!

---
+++ main.py
@@ -0,0 +1,18 @@
+# Meu primeiro projeto Python!!! 
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar uma String!(format)
+nome = "Dhénix do Nascimento"
+# Quando quiser guardar um número inteiro
+idade = 21
+
+#exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+
+''' 
+Comentario em bloco
+
+'''
